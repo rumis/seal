d72f89166a4b3c34eb0b1dbb052ecca6b6e2e680
huanjiesm <huanjiesm@163.com>
2022-08-24 14:50:29 +0800

    去掉不包含context的查询器

--- query/exector.go
+++ query/exector.go
@@ -7,12 +7,8 @@ import (
 
 // Executor prepares, executes, or queries a SQL statement.
 type Executor interface {
-	// Exec executes a SQL statement
-	Exec(query string, args ...interface{}) (sql.Result, error)
 	// ExecContext executes a SQL statement with the given context
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
-	// Query queries a SQL statement
-	Query(query string, args ...interface{}) (*sql.Rows, error)
 	// QueryContext queries a SQL statement with the given context
 	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 }
